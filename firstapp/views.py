b034863bcdb9e77146be35c8400b8744adde1993
sokoeurn88 <sokoeurn.chhaya@gmail.com>
2023-08-13 17:54:11 -0400

    first app project

---
+++ firstapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import date   
+
+# Create your views here.
+def index(request):
+    # Create a simple html page as a string
+    template = "<html>" \
+                "This is your first view" \
+                "</html>"
+    # Return template as content argument in HTTP response
+    return HttpResponse(content=template)
+
+def get_date(request):
+    today = date.today()
+    template = "<html>" \
+                "Today's date is {}" \
+                "</html>".format(today)
+    return HttpResponse(content=template)
+
